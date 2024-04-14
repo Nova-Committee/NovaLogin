2f77bf0d5571d6ab4f7f8083cfb2da1b30400fb2
cnlimiter <cnlimiter@163.com>
2024-04-14 20:21:25 +0800

    fix(multi/1.20.1): some fixs 2

--- common/src/main/java/committee/nova/mods/novalogin/mixins/ServerGamePacketListenerImplMixin.java
+++ common/src/main/java/committee/nova/mods/novalogin/mixins/ServerGamePacketListenerImplMixin.java
@@ -4,10 +4,8 @@ import committee.nova.mods.novalogin.handler.OnGameMessage;
 import committee.nova.mods.novalogin.handler.OnPlayerMove;
 import net.minecraft.network.protocol.game.ServerboundChatPacket;
 import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -24,7 +22,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerGamePacketListenerImpl.class)
 public abstract class ServerGamePacketListenerImplMixin {
 
-    @Shadow public ServerPlayer player;
     @Unique
     ServerGamePacketListenerImpl novaLogin$play =  (ServerGamePacketListenerImpl) (Object) this;
 
@@ -34,7 +31,7 @@ public abstract class ServerGamePacketListenerImplMixin {
             shift = At.Shift.AFTER
     ), cancellable = true)
     public void onPlayerMove(ServerboundMovePlayerPacket packet, CallbackInfo ci) {
-        if (!OnPlayerMove.canMove(this.player)) {
+        if (!OnPlayerMove.canMove(novaLogin$play.player)) {
             ci.cancel();
         }
     }
@@ -42,7 +39,7 @@ public abstract class ServerGamePacketListenerImplMixin {
 
     @Inject(method = "handleChat", at = @At("HEAD"), cancellable = true)
     public void onGameMessage(ServerboundChatPacket packet, CallbackInfo ci) {
-        if (!OnGameMessage.canSendMessage(this.player, packet)) {
+        if (!OnGameMessage.canSendMessage(novaLogin$play.player, packet)) {
             ci.cancel();
         }
     }
